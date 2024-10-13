135eda40d4c8d7bdf54ffce80b02b63baa59e5fb
tracer-ics2024 <tracer@njuics.org>
2024-10-13 20:31:34 +0800

    >  compile NEMU U202111035 史佳鑫 Linux LAPTOP-B5FCN1L3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:31:34 up  2:48,  1 user,  load average: 0.11, 0.16, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,12 +55,10 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-  if(args[0]=='\0') 
+  if(args == NULL) 
   cpu_exec(1);
   else 
-  {
     cpu_exec(atoi(args));
-  }
   return 0;
 }
 
