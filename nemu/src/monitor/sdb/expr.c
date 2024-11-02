8048616f78143fea6cdea6f97c852a67bda4384b
tracer-ics2024 <tracer@njuics.org>
2024-11-02 14:11:09 +0800

    >  compile NEMU U202111035 史佳鑫 Linux LAPTOP-B5FCN1L3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:11:09 up 29 min,  1 user,  load average: 0.16, 0.07, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -96,7 +96,7 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       printf("e[position] = %c\n", e[position]);
-      if (regexec(&re[i], e + position, 1, &pmatch, REG_EXTENDED) == 0 && pmatch.rm_so == 0) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
