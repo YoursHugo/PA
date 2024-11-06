772bd6e19713a45e066f548d2abce0702e168fa7
tracer-ics2024 <tracer@njuics.org>
2024-11-06 16:28:16 +0800

    >  compile NEMU U202111035 史佳鑫 Linux LAPTOP-B5FCN1L3 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:28:16 up 32 min,  1 user,  load average: 0.08, 0.10, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -181,8 +181,10 @@ u_int32_t eval(int p,int q) {
     int op= -1;//= the position of 主运算符 in the token expression;
     for(int i=p;i<=q;i++){
       if(tokens[i].type == TK_PLUS || tokens[i].type == TK_SUB || tokens[i].type == TK_MUL || tokens[i].type == TK_DIV ){
-        op = i;
-        break;
+          {
+          op = i;
+          break;
+          }
       }
     }
     u_int32_t val1 = eval(p, op - 1);
